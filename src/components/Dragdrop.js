4e5548cc9da067490e4ec5958483e747cfb5a78a
clintonjosephs <clintonjosephs26@gmail.com>
2022-01-15 15:58:16 +0100

    fixed linters errors

--- src/components/Dragdrop.js
+++ src/components/Dragdrop.js
@@ -7,7 +7,7 @@ const swapItems = (from, to) => {
   const toIndex = to - 1;
 
   const listContainer = document.querySelector('.list');
-  
+
   const toDoList = storageManager.getData();
   const itemOne = toDoList[fromIndex];
   const itemTwo = toDoList[toIndex];
@@ -16,23 +16,23 @@ const swapItems = (from, to) => {
   toDoList[fromIndex].index = from;
   toDoList[toIndex] = itemOne;
   toDoList[toIndex].index = to;
-  
-  let liFrom = listContainer.children[fromIndex].innerHTML;
-  let liTo = listContainer.children[toIndex].innerHTML;
-  
+
+  const liFrom = listContainer.children[fromIndex].innerHTML;
+  const liTo = listContainer.children[toIndex].innerHTML;
+
   listContainer.children[fromIndex].innerHTML = liTo;
   listContainer.children[fromIndex].id = from;
-  listContainer.children[fromIndex].setAttribute("data-index", from);
+  listContainer.children[fromIndex].setAttribute('data-index', from);
   listContainer.children[fromIndex].children[0].id = from;
-  listContainer.children[fromIndex].children[1].id = 'task-'+from;
-  listContainer.children[fromIndex].children[2].children[1].id = 'trash-'+from;
+  listContainer.children[fromIndex].children[1].id = `task-${from}`;
+  listContainer.children[fromIndex].children[2].children[1].id = `trash-${from}`;
 
   listContainer.children[toIndex].innerHTML = liFrom;
   listContainer.children[toIndex].id = to;
-  listContainer.children[toIndex].setAttribute("data-index", to);
+  listContainer.children[toIndex].setAttribute('data-index', to);
   listContainer.children[toIndex].children[0].id = to;
-  listContainer.children[toIndex].children[1].id = 'task-'+to;
-  listContainer.children[toIndex].children[2].children[1].id = 'trash-'+to;
+  listContainer.children[toIndex].children[1].id = `task-${to}`;
+  listContainer.children[toIndex].children[2].children[1].id = `trash-${to}`;
 
   storageManager.storeData(toDoList);
 };
